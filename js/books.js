ebe85c003cce84ea43168513859eed16ead19e5b
jdmartinezrios <jdmartinez859@misena.edu.co>
2019-01-19 13:02:12 -0500

    se suben nuevos cambios al WebSite

--- js/books.js
+++ js/books.js
@@ -62,11 +62,14 @@ function showPdf(index) {
     var pdfs10 = false;
     var pdfs11 = false;
     var pdfs12 = false;
+    var pdf;       
+    var url;
+    var options;
     if (index == 1) {
         $('#loader').show();
-        var pdf = $('#pdfView');        
-        var url = "../assets/PDF/Book/%231_20080822_Loving_God_The_First_and_Great_Commandment_TFC01.pdf";        
-        var options = {
+        pdf = $('#pdfView');        
+        url = "../assets/PDF/Book/%231_20080822_Loving_God_The_First_and_Great_Commandment_TFC01.pdf";        
+        options = {
             height: '555px'     
         };                                                                 
         pdfs = PDFObject.embed(url, pdf, options);        
@@ -74,17 +77,17 @@ function showPdf(index) {
              
     } if (index == 2) {        
         $('#loader2').show();
-        var pdf = $('#pdfView2');
-        var url = "../assets/PDF/Book/%232%2020080829_Defining_Wholehearted_Love-Four_Spheres_and_Four_Stages_TFC02.pdf";        
-        var options = {
+        pdf = $('#pdfView2');
+        url = "../assets/PDF/Book/%232%2020080829_Defining_Wholehearted_Love-Four_Spheres_and_Four_Stages_TFC02.pdf";        
+        options = {
             height: '555px'
         };
         pdfs2 = PDFObject.embed(url, pdf, options);
         (pdfs2) ?  $('#loader2').hide(1000) : $('#loader2').show();
     } if (index == 3) {        
         $('#loader3').show();
-        var pdf = $('#pdfView3');
-        var url = "../assets/PDF/Book/%233%2020080912_Defining_Wholehearted_Love_Four_Spheres_TFC03.pdf";
+        pdf = $('#pdfView3');
+        url = "../assets/PDF/Book/%233%2020080912_Defining_Wholehearted_Love_Four_Spheres_TFC03.pdf";
         options = {
             height: '555px'
         };
@@ -92,8 +95,8 @@ function showPdf(index) {
         (pdfs3) ?  $('#loader3').hide(1000) : $('#loader3').show();
     } if (index == 4) {        
         $('#loader4').show();
-        var pdf = $('#pdfView4');
-        var url = "../assets/PDF/Book/%234%2020080919_Loving_God_with_All_Our_Mind_TFC04.pdf";
+        pdf = $('#pdfView4');
+        url = "../assets/PDF/Book/%234%2020080919_Loving_God_with_All_Our_Mind_TFC04.pdf";
         options = {
             height: '555px'
         };
@@ -101,8 +104,8 @@ function showPdf(index) {
         (pdfs4) ?  $('#loader4').hide(1000) : $('#loader4').show();
     } if (index == 5) {        
         $('#loader5').show();
-        var pdf = $('#pdfView5');
-        var url = "../assets/PDF/Book/%235%2020081010_The_Relationship_of_the_First_Commandment_to_the_Second_TFC05.pdf";
+        pdf = $('#pdfView5');
+        url = "../assets/PDF/Book/%235%2020081010_The_Relationship_of_the_First_Commandment_to_the_Second_TFC05.pdf";
         options = {
             height: '555px'
         };
@@ -110,8 +113,8 @@ function showPdf(index) {
         (pdfs5) ?  $('#loader5').hide(1000) : $('#loader5').show();
     } if (index == 6) {        
         $('#loader6').show();
-        var pdf = $('#pdfView6');
-        var url = "../assets/PDF/Book/%236%2020081024_Loving_God_with_All_Our_Soul_TFC06.pdf";
+        pdf = $('#pdfView6');
+        url = "../assets/PDF/Book/%236%2020081024_Loving_God_with_All_Our_Soul_TFC06.pdf";
         options = {
             height: '555px'
         };
@@ -119,8 +122,8 @@ function showPdf(index) {
         (pdfs6) ?  $('#loader6').hide(1000) : $('#loader6').show();
     } if (index == 7) {        
         $('#loader7').show();
-        var pdf = $('#pdfView7');
-        var url = "../assets/PDF/Book/%237%2020081107_Loving_God_with_All_Our_Strength_TFC07.pdf";
+        pdf = $('#pdfView7');
+        url = "../assets/PDF/Book/%237%2020081107_Loving_God_with_All_Our_Strength_TFC07.pdf";
         options = {
             height: '555px'
         };
@@ -128,8 +131,8 @@ function showPdf(index) {
         (pdfs7) ?  $('#loader7').hide(1000) : $('#loader7').show();
     } if (index == 8) {        
         $('#loader8').show();
-        var pdf = $('#pdfView8');
-        var url = '../assets/PDF/Book/%238%2020081114_End_of_the_Age_Ministry_Paradigm_TFC08.pdf';
+        pdf = $('#pdfView8');
+        url = '../assets/PDF/Book/%238%2020081114_End_of_the_Age_Ministry_Paradigm_TFC08.pdf';
         options = {
             height: '555px'
         };
@@ -137,8 +140,8 @@ function showPdf(index) {
         (pdfs8) ?  $('#loader8').hide(1000) : $('#loader8').show();
     } if (index == 9) {        
         $('#loader9').show();
-        var pdf = $('#pdfView9');
-        var url = '../assets/PDF/Book/%239%2020081121_Practical_Ways_to_Pursue_the_First_Commandment_TFC09.pdf';
+        pdf = $('#pdfView9');
+        url = '../assets/PDF/Book/%239%2020081121_Practical_Ways_to_Pursue_the_First_Commandment_TFC09.pdf';
         options = {
             height: '555px'
         };
@@ -146,8 +149,8 @@ function showPdf(index) {
         (pdfs9) ?  $('#loader9').hide(1000) : $('#loader9').show();
     } if (index == 10) {        
         $('#loader10').show();
-        var pdf = $('#pdfView10');
-        var url = '../assets/PDF/Book/%2310%2020081128_Running_for_the_Prize_Being_Complete_in_Our_Love_TFC10.pdf';
+        pdf = $('#pdfView10');
+        url = '../assets/PDF/Book/%2310%2020081128_Running_for_the_Prize_Being_Complete_in_Our_Love_TFC10.pdf';
         options = {
             height: '555px'
         };
@@ -155,8 +158,8 @@ function showPdf(index) {
         (pdfs10) ?  $('#loader10').hide(1000) : $('#loader10').show();
     } if (index == 11) {        
         $('#loader11').show();
-        var pdf = $('#pdfView11');
-        var url = '../assets/PDF/Book/%2311%2020081130_Loving_Jesus_without_Offense_TFC11.pdf';
+        pdf = $('#pdfView11');
+        url = '../assets/PDF/Book/%2311%2020081130_Loving_Jesus_without_Offense_TFC11.pdf';
         options = {
             height: '555px'
         };
@@ -164,8 +167,8 @@ function showPdf(index) {
         (pdfs11) ?  $('#loader11').hide(1000) : $('#loader11').show();
     } if (index == 12) {        
         $('#loader12').show();
-        var pdf = $('#pdfView12');
-        var url = '../assets/PDF/Book/%2312%2020081205_Cultivating_the_Oil_of_Intimacy_TFC12.pdf';
+        pdf = $('#pdfView12');
+        url = '../assets/PDF/Book/%2312%2020081205_Cultivating_the_Oil_of_Intimacy_TFC12.pdf';
         options = {
             height: '555px'
         };
